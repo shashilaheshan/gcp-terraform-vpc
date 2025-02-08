b7ba3ef94178203ba0878c5ad69a1e14df432917
Heshan <ebayshashila@gmail.com>
2025-02-08 21:24:38 +0530

    added tf new state

---
+++ o-locals.tf
@@ -0,0 +1,10 @@
+locals {
+ project_id ="tutorial-terraform-450314"
+ region = "us-central1"
+ apis = [
+    "compute.googleapis.com",
+    "container.googleapis.com",
+    "logging.googleapis.com",
+    "secretmanager.googleapis.com"
+ ]
+}
